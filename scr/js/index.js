beec1965b24c71aeca2772deec26a9bd896e9bba
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-05-14 14:35:14 -0300

    feat: add o script para alterar as cores

--- scr/js/index.js
+++ scr/js/index.js
@@ -1,1 +1,23 @@
-console.log("HELLO");
+window.addEventListener("load", () => {
+	const darkMode = document.querySelector("#botao-alterar-tema");
+	const tagBody = document.querySelector("body");
+	const themeSave = localStorage.getItem("theme");
+	const imgTheme = document.querySelector(".imagem-botao");
+
+	if (themeSave && themeSave === "dark") {
+		tagBody.classList?.add("modo-escuro");
+		imgTheme.setAttribute("src", "./imagens/moon.png");
+	}
+
+	darkMode.addEventListener("click", () => {
+		if (!tagBody.classList?.contains("modo-escuro")) {
+			tagBody.classList?.add("modo-escuro");
+			localStorage.setItem("theme", "dark");
+			imgTheme.setAttribute("src", "./imagens/moon.png");
+		} else {
+			localStorage.setItem("theme", "ligth");
+			imgTheme.src = "./imagens/sun.png";
+			tagBody.classList?.remove("modo-escuro");
+		}
+	});
+});
